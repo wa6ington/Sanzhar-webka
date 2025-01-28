7558e566fda24eaa56dfacb515eb746d5ad465f6
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-01-28 09:25:43 +0500

    third

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-console.log('Hello Sanzh');
+import React from "react";
+import ReactDOM from 'react-dom';
+//const element = React.createElement('hi', null, 'Hello React World');
+const element = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Install React</li>
+            <li>Study React</li>
+            <li>Use React</li>
+            <li>Build React App</li>
+        </ul>
+    </div>
+    );
+    ReactDOM.render(element,
+        document.getElementById('root'));
