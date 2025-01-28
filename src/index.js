bd92e2ba33b1881826abf91384989c795847a339
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-01-28 10:14:52 +0500

    fifth

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
+import React from "react";
 import ReactDOM from 'react-dom';
 
+//const element = React.createElement('hi', null, 'Hello React World');
 const TodoList = () => {
+    const items = ['Install React', 'Study Reac', 'Use React', 'Build React App'];
     return (
         <ul>
-            <li>Install React</li>
-            <li>Study React</li>
-            <li>Use React</li>
-            <li>Build React App</li>
+        
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+            <li>{items[2]}</li>
+            <li>{items[3]}</li>
         </ul>
     );
 };
+
 const AppHeader = () =>{
     return <h1>My Todo List</h1>;
 };
+
 const SearchPanel = () => {
-    return <input placeholder="search" />;
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    // return <input placeholder="search" />;
+    return <input
+        style = {searchStyle}
+        placeholder= {searchText}
+        disabled={true} />;
 }
+
 const App = () => {
+    const value = '<script>alert ("")</script>';
+    // const isLoggedIn = false;
+    // const loginBox = <span>Log in to the system</span>;
+    // const welcomeBox = <span>Welcome Back</span>;
     return (
         <div>
-            <AppHeader />
+            {/*{ isLoggedIn ? null : loginBox }*/}
+            { value }
+
             <SearchPanel />
             <TodoList/>
-        </div>
-    );
+            </div>
+    )
 }
-// const element = <App />;
-
-    ReactDOM.render(<App />,
-      document.getElementById('root'));
