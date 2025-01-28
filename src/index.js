411da1e0e4abce99c99a1e2f6f6e12e885e27d97
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-01-28 10:11:00 +0500

    fourth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import ReactDOM from 'react-dom';
 
-<<<<<<< HEAD
-//const element = React.createElement('hi', null, 'Hello React World');
-
 const TodoList = () => {
     return (
         <ul>
@@ -11,7 +8,6 @@ const TodoList = () => {
             <li>Use React</li>
             <li>Build React App</li>
         </ul>
-  
     );
 };
 const AppHeader = () =>{
@@ -30,35 +26,6 @@ const App = () => {
     );
 }
 // const element = <App />;
-    ReactDOM.render(<App />,
-        document.getElementById('root'));
-=======
-const TodoList = () => {
-  return (
-    <ul>
-    <li>Install React</li>
-    <li>Study React</li>
-    <li>Use React</li>
-    <li>Build React App</li>
-</ul>
 
-);
-};
-const AppHeader = () =>{
-    return <h1>My Todo List</h1>;
-};
-const SearchPanel = () => {
-    return <input placeholder="search" />;
-}
-const App = () => {
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList/>
-        </div>
-    );
-}
-// const element = <App />;
-ReactDOM.render(<App />,)
->>>>>>> 3837423788e0aa1aa14d6437389a68ee7ad6b63d
+    ReactDOM.render(<App />,
+      document.getElementById('root'));
