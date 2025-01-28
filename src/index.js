3837423788e0aa1aa14d6437389a68ee7ad6b63d
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-01-28 09:29:55 +0500

    third commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from "react";
 import ReactDOM from 'react-dom';
-//const element = React.createElement('hi', null, 'Hello React World');
-const element = (
-    <div>
-        <h1>My Todo List</h1>
-        <input placeholder="search" />
-        <ul>
-            <li>Install React</li>
-            <li>Study React</li>
-            <li>Use React</li>
-            <li>Build React App</li>
-        </ul>
-    </div>
+
+const TodoList = () => {
+  return (
+    <ul>
+    <li>Install React</li>
+    <li>Study React</li>
+    <li>Use React</li>
+    <li>Build React App</li>
+</ul>
+
+);
+};
+const AppHeader = () =>{
+    return <h1>My Todo List</h1>;
+};
+const SearchPanel = () => {
+    return <input placeholder="search" />;
+}
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList/>
+        </div>
     );
-    ReactDOM.render(element,
-        document.getElementById('root'));
+}
+// const element = <App />;
+ReactDOM.render(<App />,)
