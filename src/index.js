01570380b19f1fec34685b057e5ca20e4cdd88f2
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-01-29 11:45:51 +0500

    use array like components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
-
+    const todoData = [
+        { label: 'Drink Coffee', important: false },
+        { label: 'Implement React Application', important: true },
+        { label: 'Make notes from your stud', important: false },
+        { label: 'Do not drink alchohol', important: false },
+    ];
     return (
         <div>
-     
-            <AppHeader />
-            <SearchPanel />
-            <TodoList/>
+            <TodoList todos =  {todoData} />
         </div>
     );
 }
-    ReactDOM.render(<App />,
-        document.getElementById('root'));
