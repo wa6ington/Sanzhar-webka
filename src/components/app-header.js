4a82779b0f73c3e0de6646b1ff59cabbd70f7e9d
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-02-04 09:29:45 +0500

    edit project

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -1,5 +1,14 @@
 import React from "react";
+import './app-header.css';
+const AppHeader = ({toDo, done}) => {
+    return (
+        <div className={"app-header d-flex"}>
+            <h1>Todo List</h1>
+            <h2>{toDo} more to do, {done} </h2>
+        </div>
+    );
 
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
+
 };
+
+export default AppHeader;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,21 +1,23 @@
-import React from "react";
-import TodoListItem from "./todo-list-item";
+import React from 'react';
+import TodoListItem from './todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos}) => {
-
+const TodoList = ({ todos }) => {
     const elements = todos.map((item) => {
         const { id, ...itemProps } = item;
-        return (
 
-            <li key = {id} className={"list-group-item"} >
-            <TodoListItem {...itemProps } />
-        </li>
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem {...itemProps} />
+            </li>
         );
     });
+
     return (
-        <ul className={"list-group todo-list"}>
+        <ul className="list-group todo-list">
             { elements }
         </ul>
-    );
+    ); // <-- Скобка теперь правильно закрывает return
 };
+
+export default TodoList;
