3b2f57af9c36cc671e49d77b66726318e815c46b
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-02-04 08:14:28 +0500

    rest paramater

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 
-const TodoList = ({ todos}) => {
+const TodoList= ({ todos}) => {
 
     const elements = todos.map((item) => {
+        const { id, ...itemProps } = item;
         return (
-            <li>
-            <TodoListItem {...item } />
+            <li key = { item.id } >
+            <TodoListItem {...itemProps } />
         </li>
         );
     });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+
     const todoData = [
-        { label: 'Drink Coffee', important: false },
-        { label: 'Implement React Application', important: true },
-        { label: 'Make notes from your stud', important: false },
-        { label: 'Do not drink alchohol', important: false },
+        { label: 'Drink Coffee', important: false,  id: 1},
+        { label: 'Implement React Application', important: true, id: 2 },
+        { label: 'Make notes from your stud', important: false, id: 3 },
+        { label: 'Do not drink alchohol', important: false, id: 4 },
     ];
+
     return (
         <div>
+            <AppHeader />
+            <SearchPanel />
             <TodoList todos =  {todoData} />
         </div>
     );
 }
+    ReactDOM.render(<App />,)
