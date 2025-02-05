b152bbbc03a6bdbb9f6c824e99f7ffd0b4414c4b
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-02-05 11:31:41 +0500

    constructor for todo-list-item step 19

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.onLabelClick = () => {
+            console.log(`Done: ${this.props.label}`);
+        };
+    }
+
     render() {
         const { label, important = false } = this.props;
-
+        
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
@@ -12,12 +19,12 @@ export default class TodoListItem extends Component {
 
         return (
             <span className="todo-list-item">
-                <span className="todo-list-item-label" style={style}>
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={this.onLabelClick}>
                     {label}
                 </span>
-                <button type="button" className="btn btn-outline-danger">
-                    <i className="fa fa-trash-o" />
-                </button>
             </span>
         );
     }
