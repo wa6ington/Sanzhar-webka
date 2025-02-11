6638fb764ede52f7c9afee6aac98f418d81164fb
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-02-11 10:16:24 +0500

    step 24 repair main need component with setState for events

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import AppHeader from '../app-header/app-header';
-import SearchPanel from '../search-panel/search-panel';
-import TodoList from '../todo-list/todo-list';
-import ItemStatusFilter from '../item-status-filter/item-status-filter';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
 import './app.css';
+
 const App = () => {
+
     const todoData = [
         { label: 'Drink Coffee', important: false, id: 1 },
         { label: 'Make Awesome App', important: true, id: 2 },
         { label: 'Have a lunch', important: false, id: 3 }
     ];
+
     return (
         <div className="todo-app">
             <AppHeader toDo={1} done={3} />
@@ -17,8 +22,9 @@ const App = () => {
                 <SearchPanel />
                 <ItemStatusFilter />
             </div>
-            <TodoList todos={todoData} />
+
+            <TodoList todos={todoData}
+                        onDeleted = { (id) => console.log('del', id)}/>
         </div>
     );
 };
-export default App;
