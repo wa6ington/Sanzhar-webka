d4c34056989bcad2f5b69bc65e4e380fe074addb
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-02-11 10:18:53 +0500

    step 25 deleting elements func deleteItem setState

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,24 +7,45 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
 
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ];
+export  default  class  App extends Component {
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    state = {
+     todoData : [
+            { label: 'Drink Coffee', important: false, id: 1 },
+            { label: 'Make Awesome App', important: true, id: 2 },
+            { label: 'Have a lunch', important: false, id: 3 }
+        ]
+    };
+
+
+    deleteItem = (id) => {
+        this.setState(({todoData} ) => {
+            const idx = todoData.findIndex((element) => element.id === id);
+            // console.log(idx);
+            todoData.splice(id, 1);
+            // [a, b c, d, e]
+            // [a, b,   d, e]
+            const before = todoData.slice(0, idx);
+            const after = todoData.slice(idx +1);
+            const newArray = [...before, ...after];
+            return{
+                todoData: newArray
+            };
+        });
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList todos={ this.state.todoData}
+                          onDeleted = { this.deleteItem }/>
             </div>
-
-            <TodoList todos={todoData}
-                        onDeleted = { (id) => console.log('del', id)}/>
-        </div>
-    );
-};
+        );
+    };
+}
