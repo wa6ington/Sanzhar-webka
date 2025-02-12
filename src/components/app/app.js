8d57e482b0459bd3a49c331890e6713aaa240f1d
Sanzhar Maitanov <86508061+wa6ington@users.noreply.github.com>
2025-02-12 11:18:56 +0500

    step 27 added button Add item. Adding element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,49 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import AppHeader from '../app-header';
-import SearchPanel from '../search-panel';
-import TodoList from '../todo-list';
-import ItemStatusFilter from '../item-status-filter';
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
+import ItemAddForm from "../item-add-form";
+import "./app.css";
 
-import './app.css';
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Make Awesome App", important: true, id: 2 },
+      { label: "Have a lunch", important: false, id: 3 },
+    ],
+  };
 
-export  default  class  App extends Component {
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const newArray = todoData.filter((item) => item.id !== id);
+      return {
+        todoData: newArray,
+      };
+    });
+  };
 
-    state = {
-     todoData : [
-            { label: 'Drink Coffee', important: false, id: 1 },
-            { label: 'Make Awesome App', important: true, id: 2 },
-            { label: 'Have a lunch', important: false, id: 3 }
-        ]
-    };
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
 
-    deleteItem = (id) => {
-        this.setState(({todoData} ) => {
-            const idx = todoData.findIndex((element) => element.id === id);
-            // console.log(idx);
-                const newArray = [
-                ...todoData.slice(0, idx),
-                ...todoData.slice(idx +1)
-            ];
-
-            return{
-                todoData: newArray
-            };
-        });
-    };
-
-    render() {
-        return (
-            <div className="todo-app">
-                <AppHeader toDo={1} done={3} />
-                <div className="top-panel d-flex">
-                    <SearchPanel />
-                    <ItemStatusFilter />
-                </div>
-
-                <TodoList todos={ this.state.todoData}
-                          onDeleted = { this.deleteItem }/>
-            </div>
-        );
-    };
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+        <ItemAddForm />
+      </div>
+    );
+  }
 }
